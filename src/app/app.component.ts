42c2bf3c7748e0bf102351e8db0fc82b22e4b6fa
Ali Abodaraa <abodaraaali50@gmail.com>
2023-11-29 19:23:46 +0300

    use Injection Token

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,17 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { LoggerService } from './logger.service';
 import { ExperimentalLoggerService } from './experimental-logger.service';
 import { LegacyLogger } from './logger.legacy';
+import { APP_CONFIG, AppConfig } from './config.token';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [
-    {
-      provide: LoggerService,
-      useValue: LegacyLogger,
-    },
-  ],
+  providers: [],
 })
 export class AppComponent implements OnInit {
   constructor(
@@ -22,9 +18,5 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
     this.logger.prefix = 'App Component';
     this.logger.log('App Component Init ...');
-    console.log(
-      'is instance the same : ',
-      this.logger === this.experimentlLogger
-    );
   }
 }
--- src/app/experimental-logger.service.ts
+++ src/app/experimental-logger.service.ts
@@ -1,12 +1,15 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
 import { Logger } from './logger';
+import { APP_CONFIG, AppConfig } from './config.token';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ExperimentalLoggerService implements Logger {
   prefix = 'root';
-  constructor() {}
+  constructor(@Inject(APP_CONFIG) private config: AppConfig) {
+    console.log('ExperimentalLoggerService => constructor => config', config);
+  }
   log(mssg: string): void {
     console.log(`${this.prefix} : (Experimental) ${mssg}`);
   }
