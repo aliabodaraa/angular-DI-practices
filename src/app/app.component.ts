52626a1107dc67e6b1a12a2fba8532d4a18850a0
Ali Abodaraa <abodaraaali50@gmail.com>
2023-11-29 17:58:34 +0300

    useClass

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LoggerService } from './logger.service';
+import { ExperimentalLoggerService } from './experimental-logger.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [
+    {
+      provide: LoggerService,
+      useClass: ExperimentalLoggerService,
+    },
+  ],
 })
-export class AppComponent {
-  title = 'learn';
+export class AppComponent implements OnInit {
+  constructor(
+    private logger: LoggerService,
+    private experimentlLogger: ExperimentalLoggerService
+  ) {}
+  ngOnInit(): void {
+    this.logger.prefix = 'App Component';
+    this.logger.log('App Component Init ...');
+    console.log(
+      'is instance the same : ',
+      this.logger === this.experimentlLogger
+    );
+  }
 }
--- src/app/logger.service.ts
+++ src/app/logger.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Logger } from './logger';
 
 @Injectable({
   providedIn: 'root',
 })
-export class LoggerService {
+export class LoggerService implements Logger {
+  prefix = 'root';
+  constructor() {}
+  log(mssg: string): void {
+    console.log(`${this.prefix} : ${mssg}`);
+  }
   logInfo(msg: any) {
     console.log('logInfo', msg);
   }
